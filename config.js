3edb22fda915105a1cbbbed487efe76cdc5bdc2d
ajiupengyou <84163488+ajiupengyou@users.noreply.github.com>
2022-08-22 13:44:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ const config = {
 
     AppSecret: "eb00852b8025c75ccdf22b2e98a3e333", //公众号里面取
 
-    base_templateId: "", // 元气提醒模板的id
+    base_templateId: "	62Fm7xo1JHAA6KMASxxplsU-gdEKvkwtMQdCChATtEM", // 元气提醒模板的id
 
-    birthday_templateId: "", // 生日模板id
+    birthday_templateId: "	uFoJDauJy-uaOi5-4tmFNgO10KjD6kEpXanvRd8TAQ0", // 生日模板id
 
-    pay_templateId: "", // 发薪模板id
+    //pay_templateId: "", // 发薪模板id
 
-    love_templateId: "", // 相恋模板id
+    love_templateId: "	l5poj1lAlB8jjh9gVqPXGyYIE3EwKK0JNM66y-UMSWc", // 相恋模板id
 
     token: "jimmyxuexue", //自定义的token
   },
